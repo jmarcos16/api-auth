e9fc3eb72d38de8498ceb990b78411ed7c8061eb
jmarcos16 <contato.marcosrf@gmail.com>
2023-03-23 17:07:12 -0300

    Api auth

--- routes/api.php
+++ routes/api.php
@@ -1,41 +1,19 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\UserController;
-use App\Models\User;
 
-Route::controller(RegisterController::class)->group(function () {
-  Route::post('register', 'register');
-  Route::post('login', 'login');
-});
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
 
-Route::post('/authenticate', [AuthController::class, 'auth']);
-
-Route::controller(UserController::class)->group(function () {
-  Route::get('users', 'index');
-  Route::post('store', 'store');
-  Route::get('show/{id}', 'show');
-  Route::put('update/{id}', 'update');
-  Route::delete('destroy/{id}', 'destroy');
-});
-
-Route::middleware('auth.sanctum')->group(function () {
-  Route::resource('products', ProductController::class);
-});
-
-Route::get('teste-something-weird', function () {
-
-  $user =  new User;
-  $id = 10;
-
-  $user->givePermission('dedwdwadawafault', $id);
-
-  $user = User::find($id);
-
-  // dd($user->permission()->where('roles', 'dedwdwadawafault')->exists());
-  return response()->json(['test']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
