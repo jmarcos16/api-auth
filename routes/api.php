70708879ba6f0d677661840169c41bb01c3af850
jmarcos16 <contato.marcosrf@gmail.com>
2023-03-27 17:03:31 -0300

    Organizing application routes

--- routes/api.php
+++ routes/api.php
@@ -2,41 +2,19 @@
 
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\UserController;
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::middleware('permission:admin')->group(function () {
+  Route::post('set-permission/{user_id}', [PermissionController::class, 'store']);
+  Route::post('delete-permission/{user_id}', [PermissionController::class, 'destroy']);
 
-Auth::login(User::find(1));
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-  return $request->user();
-});
-
-Route::controller(UserController::class)->group(function () {
-  Route::get('users', 'index');
-  Route::post('store', 'store');
-  Route::get('user/{id}', 'show');
-  Route::post('update/{id}', 'update');
-  Route::delete('delete/{id}', 'destroy');
+  Route::post('update/{id}', [UserController::class, 'update']);
+  Route::post('store', [UserController::class, 'store']);
+  Route::delete('delete/{id}', [UserController::class, 'destroy']);
 });
 
 
-Route::post('set-permission/{user_id}', [PermissionController::class, 'store']);
-Route::post('delete-permission/{user_id}', [PermissionController::class, 'destroy']);
-
-
-// Route::get('teste', function () {
-//   dd('oi');
-// })->middleware('permission:admin');
+Route::middleware('permission:default')->group(function () {
+  Route::get('users', [UserController::class, 'index']);
+  Route::get('user/{id}', [UserController::class, 'show']);
+});
